d75c7389d29d380d8d52c92b33d8e1e98e9d56c3
澤田皇汰 <sawadakota@ksawada.local>
2015-10-17 09:17:06 +0900

    post photo function except photo,tag

--- protospace/app/controllers/proto_controller.rb
+++ protospace/app/controllers/proto_controller.rb
@@ -1,8 +1,18 @@
 class ProtoController < ApplicationController
-
-  def ranking
+  def index
   end
 
-  def newest
+  def new
+    @proto = Proto.new
+  end
+
+  def create
+    Proto.create(proto_params)
+    redirect_to controller: :proto, action: :ranking
+  end
+
+  private
+  def proto_params
+    params.require(:proto).permit(:title, :concept, :catch_copy)
   end
 end
--- protospace/config/routes.rb
+++ protospace/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "proto#ranking"
-  resources :proto, only: [:index, :new]
+  resources :proto, only: [:index, :new, :create]
 end
