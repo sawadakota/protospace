e71dfff7c264763acdd7e82a2bb540c6e00202c4
澤田皇汰 <sawadakota@ksawada.local>
2015-10-17 17:38:33 +0900

    delete mystery root

--- protospace/config/routes.rb
+++ protospace/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  namespace :hoge do
-  get 'users/index'
-  end
-
   devise_for :users
   root "protos#ranking"
   resources :protos, only: [:index, :new, :create]
