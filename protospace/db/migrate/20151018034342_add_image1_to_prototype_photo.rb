1dde00ddf7c6e0edcf8c40fa8abfd2f3b4ea92d6
澤田皇汰 <sawadakota@ksawada.local>
2015-10-19 12:29:31 +0900

    Revert "add image column to prototype_image table"
    This reverts commit 466c3dd8a4c5a98f0fe720d69b9e1e3dd9fcb379.

--- protospace/db/migrate/20151018034342_add_image1_to_prototype_photo.rb
+++
@@ -1,7 +0,0 @@
-class AddImage1ToPrototypePhoto < ActiveRecord::Migration
-  def change
-    add_column :prototype_photos, :image1, :string
-    add_column :prototype_photos, :image2, :string
-    add_column :prototype_photos, :image3, :string
-  end
-end
