90b1c11c86f375d49b9bc802c1f553d7c50e7b5a
澤田皇汰 <sawadakota@ksawada.local>
2015-10-20 02:38:41 +0900

    notation change

--- protospace/db/migrate/20151018012236_create_prototype_photos.rb
+++
@@ -1,9 +0,0 @@
-class CreatePrototypePhotos < ActiveRecord::Migration
-  def change
-    create_table :prototype_photos do |t|
-      t.references :proto
-
-      t.timestamps null: false
-    end
-  end
-end
