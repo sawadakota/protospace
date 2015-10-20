241df24d24555fa2efeada4ab3752383b4266e09
澤田皇汰 <sawadakota@ksawada.local>
2015-10-20 17:18:22 +0900

    user edit function

--- protospace/app/controllers/users_controller.rb
+++ protospace/app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def update
     current_user.update(update_params)
+    redirect_to controller: 'protos', action: 'ranking'
   end
 
   private
--- protospace/config/routes.rb
+++ protospace/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "protos#ranking"
   resources :protos, only: [:index, :new, :create]
-  resources :users, only: [:edit]
+  resources :users, only: [:edit, :update]
 end
