491bbcb61cd73be5b736ee48fee6fc8f58533b11
澤田皇汰 <sawadakota@ksawada.local>
2015-11-14 11:18:34 +0900

    image preview

---
+++ protospace/app/assets/javascripts/thumbnail/thumbnails.js
@@ -0,0 +1,15 @@
+$(window).on('page:change',function() {
+      // jQuery Upload uploadThumbs
+      $('#preview input:file').uploadThumbs({
+      });
+      $('#sub1_preview input:file').uploadThumbs({
+      });
+      $('#sub2_preview input:file').uploadThumbs({
+      });
+      $('#preview input:file').updateThumbs
+      $('#sub1_preview input:file').updateThumbs
+      $('#sub2_preview input:file').updateThumbs
+      $('#name input:file').uploadThumbs({
+      });
+      $('#name input:file').updateThumbs
+  });
